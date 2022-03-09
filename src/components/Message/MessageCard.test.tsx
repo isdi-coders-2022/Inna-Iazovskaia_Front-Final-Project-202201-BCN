20d80b3a498a2b15371c8e0223682bc0ab5950a8
Inna Iazovkaia <inna.0961@mail.ru>
2022-03-09 20:21:03 +0100

    Corrected import of MessageCard in MessageCard.test.tsx

--- src/components/Message/MessageCard.test.tsx
+++ src/components/Message/MessageCard.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import MessageCard from "./Message";
+import MessageCard from "./MessagCard";
 
 describe("Given a Message component", () => {
   describe("When it's receives message with 'Hello' as text", () => {
