5ce64ecaa6e006a1a73e84eb9a27653fa3f95593
Inna Iazovkaia <inna.0961@mail.ru>
2022-03-10 22:33:50 +0100

    Added property id in objects type Message

--- src/rudux/mocks/handlers.ts
+++ src/rudux/mocks/handlers.ts
@@ -4,20 +4,24 @@ export const handlers = [
   rest.get(`${process.env.REACT_APP_API_FINDME}messages/all`, (req, res, ctx) =>
     res(
       ctx.status(200),
-      ctx.json([
-        {
-          date: "",
-          text: "Hello!",
-          sender: "",
-          recipient: "",
-        },
-        {
-          date: "",
-          text: "How are you?",
-          sender: "",
-          recipient: "",
-        },
-      ])
+      ctx.json({
+        messages: [
+          {
+            date: "",
+            text: "Hello!",
+            sender: "",
+            recipient: "",
+            id: "1",
+          },
+          {
+            date: "",
+            text: "How are you?",
+            sender: "",
+            recipient: "",
+            id: "2",
+          },
+        ],
+      })
     )
   ),
 ];
