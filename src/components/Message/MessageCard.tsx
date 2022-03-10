a2a338c6c7765743c9f57d16b87c8cd5d8117313
Inna Iazovkaia <inna.0961@mail.ru>
2022-03-10 22:39:34 +0100

    Added styles for MessageCard in MessageCard.tsx

--- src/components/Message/MessageCard.tsx
+++ src/components/Message/MessageCard.tsx
@@ -1,11 +1,22 @@
 import Message from "../../types/Message";
+import styled from "styled-components";
 
 interface MessageCardProps {
   message: Message;
 }
 
+const MessageContainer = styled.li`
+  color: white;
+  width: 210px;
+  background-color: rgba(237, 1, 127, 1);
+  border-radius: 20px;
+  padding: 15px;
+  margin-bottom: 10px;
+  font-size: 18px;
+`;
+
 const MessageCard = ({ message: { text } }: MessageCardProps): JSX.Element => {
-  return <li>{text}</li>;
+  return <MessageContainer>{text}</MessageContainer>;
 };
 
 export default MessageCard;
