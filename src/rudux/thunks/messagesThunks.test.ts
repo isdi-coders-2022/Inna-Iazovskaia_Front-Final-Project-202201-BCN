3300004f62ab3e72a6fb63f8c58813505c12da11
Inna Iazovkaia <inna.0961@mail.ru>
2022-03-12 16:35:00 +0100

    Created test for deleteMessageThunk in messagesThunks.test.ts

--- src/rudux/thunks/messagesThunks.test.ts
+++ src/rudux/thunks/messagesThunks.test.ts
@@ -1,4 +1,4 @@
-import { loadMessagesThunk } from "./messagesThunks";
+import { deleteMessageThunk, loadMessagesThunk } from "./messagesThunks";
 
 describe("Civen a loadMessagesThunk function", () => {
   describe("When it's called", () => {
@@ -6,6 +6,20 @@ describe("Civen a loadMessagesThunk function", () => {
       const dispatch = jest.fn();
 
       await loadMessagesThunk(dispatch);
+
+      expect(dispatch).toHaveBeenCalled();
+    });
+  });
+});
+
+describe("Given a deleteMessageThunk function", () => {
+  describe("When it's called with id of deleting message", () => {
+    test("Then it should call dispatch and pass deleteMessageAction", async () => {
+      const id = "1";
+      const dispatch = jest.fn();
+
+      const deleteThunk = deleteMessageThunk(id);
+      await deleteThunk(dispatch);
 
       expect(dispatch).toHaveBeenCalled();
     });
