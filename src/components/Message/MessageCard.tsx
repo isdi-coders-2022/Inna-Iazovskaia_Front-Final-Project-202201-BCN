e9db22d96d201601ab4919070ed3daa8707268ef
Inna Iazovkaia <inna.0961@mail.ru>
2022-03-12 19:02:25 +0100

    Added delete icon in MessageCard.tsx

--- src/components/Message/MessageCard.tsx
+++ src/components/Message/MessageCard.tsx
@@ -1,11 +1,24 @@
 import Message from "../../types/Message";
 import styled from "styled-components";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 interface MessageCardProps {
   message: Message;
+  actionOnClick: React.MouseEventHandler;
 }
 
 const MessageContainer = styled.li`
+  display: flex;
+  align-items: center;
+
+  .page_icon {
+    color: white;
+    margin-left: 15px;
+    font-size: 20px;
+  }
+`;
+
+const MessageText = styled.p`
   color: white;
   width: 210px;
   background-color: rgba(237, 1, 127, 1);
@@ -15,8 +28,21 @@ const MessageContainer = styled.li`
   font-size: 18px;
 `;
 
-const MessageCard = ({ message: { text } }: MessageCardProps): JSX.Element => {
-  return <MessageContainer>{text}</MessageContainer>;
+const MessageCard = ({
+  message: { text, id },
+  actionOnClick,
+}: MessageCardProps): JSX.Element => {
+  return (
+    <MessageContainer>
+      <MessageText>{text}</MessageText>
+      <FontAwesomeIcon
+        className="page_icon"
+        icon={faTrashCan}
+        onClick={actionOnClick}
+        data-testid="deleteIcon"
+      />
+    </MessageContainer>
+  );
 };
 
 export default MessageCard;
