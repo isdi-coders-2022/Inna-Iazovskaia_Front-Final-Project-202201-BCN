3a4193096f6811d09fab4bd35e23c6f4f6d1aea5
Inna Iazovkaia <inna.0961@mail.ru>
2022-03-12 16:32:08 +0100

    Created deleteMessageThunk in messagesThunks.ts

--- src/rudux/thunks/messagesThunks.ts
+++ src/rudux/thunks/messagesThunks.ts
@@ -1,5 +1,8 @@
 import { ThunkDispatch } from "redux-thunk";
-import { loadMessagesAction } from "../actions/actionsCreators";
+import {
+  deleteMessageAction,
+  loadMessagesAction,
+} from "../actions/actionsCreators";
 import { AnyAction } from "redux";
 
 export const loadMessagesThunk = async (
@@ -12,3 +15,16 @@ export const loadMessagesThunk = async (
   const messages = messagesList.messages;
   dispatch(loadMessagesAction(messages));
 };
+
+export const deleteMessageThunk =
+  (id: string) => async (dispatch: ThunkDispatch<void, unknown, AnyAction>) => {
+    const response = await fetch(
+      `${process.env.REACT_APP_API_FINDME}messages/delete/${id}`,
+      {
+        method: "DELETE",
+      }
+    );
+    if (response.ok) {
+      dispatch(deleteMessageAction(id));
+    }
+  };
