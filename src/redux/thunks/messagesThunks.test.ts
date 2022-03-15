eb8bad1bf505a5bce8877dd264fdd6ab60bf18fa
Inna Iazovkaia <inna.0961@mail.ru>
2022-03-15 20:46:40 +0100

    Added test for deleteMessageThunk in messagesThunks.test.ts

--- src/redux/thunks/messagesThunks.test.ts
+++ src/redux/thunks/messagesThunks.test.ts
@@ -17,7 +17,7 @@ describe("Civen a loadMessagesThunk function", () => {
 });
 
 describe("Given a deleteMessageThunk function", () => {
-  describe("When it's called with id of deleting message", () => {
+  describe("When it's called with id 1 of deleting message", () => {
     test("Then it should call dispatch and pass deleteMessageAction", async () => {
       const id = "1";
       const dispatch = jest.fn();
@@ -33,10 +33,22 @@ describe("Given a deleteMessageThunk function", () => {
       expect(dispatch).toHaveBeenCalledWith(expectedAction);
     });
   });
+
+  describe("When it's called with id 2", () => {
+    test("Then it shouldn't call dispatch", async () => {
+      const id = "2";
+      const dispatch = jest.fn();
+
+      const deleteThunk = deleteMessageThunk(id);
+      await deleteThunk(dispatch);
+
+      expect(dispatch).not.toBeCalled();
+    });
+  });
 });
 
 describe("Given a createMessageThunk function", () => {
-  describe("When it's called with message", () => {
+  describe("When it's called with message 'I am fine.'", () => {
     test("Then it should call dispatch and pass createMessageAction", async () => {
       const message = {
         date: "",
