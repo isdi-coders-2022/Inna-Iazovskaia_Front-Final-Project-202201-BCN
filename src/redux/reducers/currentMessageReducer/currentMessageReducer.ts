eee2cf9c44e38be6927fa7692cb02c6aa2eb3188
Inna Iazovkaia <inna.0961@mail.ru>
2022-03-18 21:07:03 +0100

    Created currentMessageReducer in currentMessageReducer.ts

---
+++ src/redux/reducers/currentMessageReducer/currentMessageReducer.ts
@@ -0,0 +1,21 @@
+import { AnyAction } from "redux";
+import Message from "../../../types/Message";
+import actionsTypes from "../../actions/actionsTypes";
+import defaultMessage from "./defaultMessage";
+
+const currentMessageReducer = (
+  currentMessage: Message = defaultMessage,
+  action: AnyAction
+): Message => {
+  let newCurrentMessage: Message;
+
+  if (action.type === actionsTypes.loadCurrentMessage) {
+    newCurrentMessage = { ...action.message };
+  } else {
+    newCurrentMessage = { ...currentMessage };
+  }
+
+  return newCurrentMessage;
+};
+
+export default currentMessageReducer;
