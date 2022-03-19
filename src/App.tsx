4711326353962640d35af0ffa63660a9ab5bd121
Inna Iazovkaia <inna.0961@mail.ru>
2022-03-19 16:26:44 +0100

    Created Route for MessageDetailsPage in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
+import MessageDetailsPage from "./pages/MessageDetailsPage/MessageDetailsPage";
 import MessagesPage from "./pages/MessagesPage/MessagesPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 import UpdateMessagePage from "./pages/UpdateMessagePage/UpdateMessagePage";
@@ -15,6 +16,7 @@ function App() {
           <Route path="/profile" element={<NotFoundPage />} />
           <Route path="/conversations" element={<MessagesPage />} />
           <Route path="/update-message/:id" element={<UpdateMessagePage />} />
+          <Route path="/message/:id" element={<MessageDetailsPage />} />
         </Routes>
       </main>
     </>
