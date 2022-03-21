b114a360071713cdd29b5bdb61d43380dd1d16aa
Inna Iazovkaia <inna.0961@mail.ru>
2022-03-21 17:50:54 +0100

    Imported serReduser into src/redux/reducers/index.ts

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import currentMessageReducer from "./currentMessageReducer/currentMessageReducer";
 import messagesReducer from "./messagesReducer/messagesReducer";
+import userReduser from "./userReduser/userReducer";
 
 const rootReducer = combineReducers({
   messages: messagesReducer,
   message: currentMessageReducer,
+  user: userReduser,
 });
 
 export default rootReducer;
