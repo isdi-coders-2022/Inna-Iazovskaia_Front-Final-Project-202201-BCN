2e64baf3545e9364de0bc3e2c71e70f2da92a101
Inna Iazovkaia <inna.0961@mail.ru>
2022-03-22 17:38:22 +0100

    Deleted Navigation and created route /register-page in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import Navigation from "./components/Navigation/Navigation";
+import LoginPage from "./pages/LoginPage/LoginPage";
 import MessageDetailsPage from "./pages/MessageDetailsPage/MessageDetailsPage";
 import MessagesPage from "./pages/MessagesPage/MessagesPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
@@ -9,12 +9,12 @@ function App() {
   return (
     <>
       <main>
-        <Navigation />
         <Routes>
-          <Route path="/" element={<MessagesPage />} />
+          <Route path="/" element={<LoginPage />} />
           <Route path="/home" element={<NotFoundPage />} />
           <Route path="/profile" element={<NotFoundPage />} />
           <Route path="/conversations" element={<MessagesPage />} />
+          <Route path="/register-page" element={<NotFoundPage />} />
           <Route path="/update-message/:id" element={<UpdateMessagePage />} />
           <Route path="/message/:id" element={<MessageDetailsPage />} />
         </Routes>
